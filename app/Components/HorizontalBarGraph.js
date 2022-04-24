037257c394979b9b2732b3cc10038566e6980bf1
Anca Puscasu <ancapuscasu@ymail.com>
2022-04-24 19:23:11 -0400

    Created HorizontalBarGraph component

---
+++ app/Components/HorizontalBarGraph.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native'
+import { VictoryBar } from 'victory';
+
+function HorizontalBarGraph(props) {
+    return (
+        <VictoryBar/>
+}
+
+const styles = StyleSheet.create({
+    
+})
+export default HorizontalBarGraph;
