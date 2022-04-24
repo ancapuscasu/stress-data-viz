2c20d249f4ecdaccb0fa20d84dd0993669e24a30
Anca Puscasu <ancapuscasu@ymail.com>
2022-04-24 17:00:38 -0400

    color constants for the project

---
+++ app/config/colors.js
@@ -0,0 +1,17 @@
+export default {
+  //brand colors
+  green: "#3A973A",
+  orange: "#E96425",
+  blue: "#36BDC4",
+  red: "#F55558",
+
+  //gradient colors
+  greenGra: "#a1fe00",
+  redGra: "#ff0000",
+
+  white: "#fff",
+  black: "#000",
+  lightGrey: "#F8F8F8",
+  mediumGrey: "#737373",
+  darkGrey: "#474747",
+};
