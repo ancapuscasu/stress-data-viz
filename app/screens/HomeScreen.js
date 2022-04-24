44544baff52acfec2ae1afd99f0e8b484b3263e5
Anca Puscasu <ancapuscasu@ymail.com>
2022-04-24 19:25:17 -0400

    Created HomeScreen and imported userData from assets

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 
+import { userData } from "../assets/userData";
+import CurrentState from "../Components/CurrentState";
 import Screen from "../Components/Screen";
+import colors from "../config/colors";
 
 function HomeScreen() {
-  return <Screen></Screen>;
+  const DATA = JSON.parse(JSON.stringify(userData));
+
+  return (
+    <Screen style={styles.container}>
+      <CurrentState style={styles.currentState} userData={DATA} />
+    </Screen>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.lightGrey,
+  },
+  currentState: {
+    backgroundColor: colors.white,
+  },
+});
 
 export default HomeScreen;
