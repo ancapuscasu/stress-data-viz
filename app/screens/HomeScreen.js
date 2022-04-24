8847f2feb47e4629b230a024634fe86564f9417e
Anca Puscasu <ancapuscasu@ymail.com>
2022-04-24 17:01:30 -0400

    Created HomeScreen
    will have CurrentState graph and DailySummaryGraph

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+import Screen from "../Components/Screen";
+
+function HomeScreen() {
+  return <Screen></Screen>;
+}
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
