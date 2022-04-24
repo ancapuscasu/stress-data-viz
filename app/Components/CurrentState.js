183f304528af7f74696d4080c69ab52b8345f7a5
Anca Puscasu <ancapuscasu@ymail.com>
2022-04-24 19:22:49 -0400

    Added userData prop from HomeScreen and added function to get time ago

--- app/Components/CurrentState.js
+++ app/Components/CurrentState.js
@@ -1,28 +1,72 @@
+/**
+ * Component shows the current anxietyState, how long ago the state was updated and the
+ * CurrentStateGraph gradient meter to visually show the anxietyState
+ */
+
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import moment from "moment";
 
 import AppText from "./AppText";
 import CurrentStateGraph from "./CurrentStateGraph";
-import { userData } from "../assets/userData";
+import colors from "../config/colors";
 
-function CurrentState(props) {
+function CurrentState({ userData, style }) {
+  // Most recent object (log) from userData array
+  const lastLog = userData[0];
+
+  //Function to get time ago of the latest log (compares updatedAt time to current time)
+  const lastUpdate = moment.utc(`${lastLog.updatedAt.$date}`).local().fromNow();
+
   return (
-    <View style={styles.container}>
-      <AppText style={styles.stressStatus}>hello</AppText>
-      <AppText style={styles.stressStatus}>High</AppText>
-      <CurrentStateGraph />
+    <View style={[styles.container, style]}>
+      <View style={styles.messageContainer}>
+        <AppText style={styles.stressStatus}>{lastLog.anxietyState}</AppText>
+        <AppText style={styles.updateMsg}>Last updated {lastUpdate}</AppText>
+      </View>
+      <CurrentStateGraph style={styles.graph} lastLog={lastLog} />
+      <View style={styles.button}>
+        <AppText style={styles.buttonText}>Add A Note</AppText>
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.orange,
+    width: 250,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    marginBottom: 45,
+  },
+  buttonText: {
+    fontWeight: "400",
+    color: colors.white,
+  },
   container: {
     justifyContent: "center",
     alignItems: "center",
+    borderRadius: 15,
+    margin: 10,
+  },
+  graph: {
+    marginBottom: 45,
+    margin: 20,
+  },
+  messageContainer: {
+    alignItems: "center",
+    marginTop: 20,
+    marginBottom: 45,
   },
   stressStatus: {
     fontWeight: "500",
-    fontSize: 40,
+    fontSize: 55,
+    textTransform: "capitalize",
+  },
+  updateMsg: {
+    fontSize: 18,
   },
 });
 
