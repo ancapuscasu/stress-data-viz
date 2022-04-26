fd200081c3baabb37afe658d3a6adbc271d4615e
Anca Puscasu <ancapuscasu@ymail.com>
2022-04-25 20:56:13 -0400

    cleaned up code

--- app/Components/Screen.js
+++ app/Components/Screen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Platform, StatusBar } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import Constants from "expo-constants";
 
 function Screen({ children, style }) {
